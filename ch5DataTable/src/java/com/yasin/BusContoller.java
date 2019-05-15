93770d0bd7f658fc9bb3c1fd2a806e839b078ee9
mdyasinarif <mdyasinarifidb49@gmail.com>
2019-05-15 16:33:24 +0600

    ddfuoeciudgfjkuolfdsgfdgfdsfds

---
+++ ch5DataTable/src/java/com/yasin/BusContoller.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.yasin;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author Students
+ */
+@ManagedBean
+public class BusContoller {
+    private List<Bus> busList;
+
+    public List<Bus> getBusList() {
+        busList = new ArrayList<>();
+        busList.add(new Bus("100", "1980", "Toyto", "Red"));
+        busList.add(new Bus("101", "1990", "Nano", "Green"));
+        busList.add(new Bus("102", "2010", "Dhakar Chaka", "Blue"));
+        busList.add(new Bus("103", "2019", "BRTC Charakar", "Red"));
+        busList.add(new Bus("104", "2015", "Bandu", "Kala"));
+        busList.add(new Bus("105", "2019", "Khandu", "Sada"));
+        busList.add(new Bus("106", "1920", "Xkorola", "White"));
+        busList.add(new Bus("107", "2016", "BMW", "Black"));
+        return busList;
+    }
+
+    public void setBusList(List<Bus> busList) {
+        this.busList = busList;
+    }
+    
+    
+}
